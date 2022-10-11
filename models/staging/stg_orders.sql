73e0562c0b216c2a5be12734ba7283169aebdf41
ujjwal.kumar@thoughtworks.com <ujjwal.kumar@thoughtworks.com>
2022-10-11 11:34:04 +0000

    developed report_profit_customer and report_profit_by_product

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -15,13 +15,16 @@ o.shipdate,
 o.shipmode,
 o.ordersellingprice - o.ordercostprice as orderprofit,
 -- from raw_customer
+c.customerid,
 c.customername,
 c.segment,
 c.country,
+
 -- from raw_product
 p.category,
 p.productname,
-p.subcategory
+p.subcategory,
+p.productid
 from {{ ref('raw_orders') }} as o 
 left join {{ ref('raw_customer') }} as c 
 on o.customerid = c.customerid

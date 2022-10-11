46ac9facdff3b44d474d7fa89a903668329098ae
ujjwal.kumar@thoughtworks.com <ujjwal.kumar@thoughtworks.com>
2022-10-11 11:09:19 +0000

    developing stg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,29 @@
+/*
+
+select *, 
+ordersellingprice - ordercostprice as orderprofit 
+from RAW.GLOBALMART.ORDERS 
+-- one line comment in dbt
+from {{ ref('raw_orders') }}
+*/
+
+select 
+-- from raw_orders
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+o.ordersellingprice - o.ordercostprice as orderprofit,
+-- from raw_customer
+c.customername,
+c.segment,
+c.country,
+-- from raw_product
+p.category,
+p.productname,
+p.subcategory
+from {{ ref('raw_orders') }} as o 
+left join {{ ref('raw_customer') }} as c 
+on o.customerid = c.customerid
+left join {{ ref('raw_product') }} as p 
+on o.productid = p.productid
